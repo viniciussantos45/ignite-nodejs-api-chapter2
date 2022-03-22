d3d36bab2c5a1c9633a2672aa7cfd595c0b60519
Vinicius Gomes <vinigomes.0311@gmail.com>
2022-03-21 22:03:31 -0300

    Upload e leitura de arquivo; Documentação com Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerSetup from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerSetup));
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
